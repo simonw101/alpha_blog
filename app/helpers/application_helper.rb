a56d264fd90eee2edabd52e709c9ded027581af2
simonw101 <simonw101@googlemail.com>
2021-11-23 07:31:17 -0500

    Add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+    def gravatar_for(user, options = { size: 80 })
+        email_address = user.email.downcase
+
+        hash = Digest::MD5.hexdigest(email_address)
+
+        # gravatar_url = "https://s.gravatar.com/avatar/#{hash}"
+
+        size = options[:size]
+
+        gravatar_url = "https://s.gravatar.com/avatar/d2e64fb49185d6b8295d7cc71d4b0e32?s=#{size}"
+
+        image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+    end
+
 end
